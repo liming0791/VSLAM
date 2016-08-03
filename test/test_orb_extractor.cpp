77bcd449913160544f924c2e9833421ca130a5c0
liming <liming@rokid.com>
2016-08-03 19:04:49 +0800

    try to fix bug of triangulation

--- test/test_orb_extractor.cpp
+++ test/test_orb_extractor.cpp
@@ -8,10 +8,15 @@
 #include "opencv2/core/core.hpp"
 #include "opencv2/nonfree/nonfree.hpp"
 
+#include "Eigen/Dense"
+
 #include "ORBextractor.h"
 #include "ORBmatcher.h"
 #include "PnPSolver.hpp"
 #include "PCLView.hpp"
+
+using namespace std;
+using namespace cv;
 
 std::string getImageType(cv::Mat &mat)
 {
@@ -179,13 +184,240 @@ void getPointsFromHomogeneous(cv::Mat &h, std::vector<cv::Point3f> &pts){
     }
 }
 
-bool Point3fRight(std::vector<cv::Point3f> &pt3f){
+bool Point3fRight(std::vector<cv::Point3f> &pt3f, cv::Mat &projMat){
     for(int i = 0, _end = pt3f.size(); i < _end; i++){
-        if(pt3f[i].z<0){
+        if(pt3f[i].z<0 || (projMat.at<float>(8)*pt3f[i].x + projMat.at<float>(9)*pt3f[i].y + projMat.at<float>(10)*pt3f[i].z + projMat.at<float>(11))<0){
             return false;
         }
     }
     return true;
+}
+
+Mat_<float> LinearLSTriangulation(Point3f u,       //homogenous image point (u,v,1)
+        Matx34f P,       //camera 1 matrix
+        Point3f u1,      //homogenous image point in 2nd camera
+        Matx34f P1       //camera 2 matrix
+        )
+{
+    //build matrix A for homogenous equation system Ax = 0
+    //assume X = (x,y,z,1), for Linear-LS method
+    //which turns it into a AX = B system, where A is 4x3, X is 3x1 and B is 4x1
+    
+
+    //solve using eigen
+    //Eigen::MatrixXf A(4,3);
+    //A << u.x*P(2, 0) - P(0, 0), u.x*P(2, 1) - P(0, 1), u.x*P(2, 2) - P(0, 2),
+    //     u.y*P(2, 0) - P(1, 0), u.y*P(2, 1) - P(1, 1), u.y*P(2, 2) - P(1, 2),
+    //     u1.x*P1(2, 0) - P1(0, 0), u1.x*P1(2, 1) - P1(0, 1), u1.x*P1(2, 2) - P1(0, 2),
+    //     u1.y*P1(2, 0) - P1(1, 0), u1.y*P1(2, 1) - P1(1, 1), u1.y*P1(2, 2) - P1(1, 2);
+    //Eigen::Vector4f B;
+    //B << -(u.x*P(2, 3) - P(0, 3)),
+    //     -(u.y*P(2, 3) - P(1, 3)),
+    //     -(u1.x*P1(2, 3) - P1(0, 3)),
+    //     -(u1.y*P1(2, 3) - P1(1, 3));
+    //Eigen::Vector3f x = A.colPivHouseholderQr().solve(B);
+    //Mat_<float> X(3,1, CV_32FC1);
+    //X.at<float>(0) = x[0];
+    //X.at<float>(1) = x[1];
+    //X.at<float>(2) = x[2];
+    //solve using eigen done
+    //
+    //solve using opencv
+    Matx43f A((u.x*P(2, 0) - P(0, 0)) , (u.x*P(2, 1) - P(0, 1)) , (u.x*P(2, 2) - P(0, 2)) ,
+            (u.y*P(2, 0) - P(1, 0)) , (u.y*P(2, 1) - P(1, 1)) , (u.y*P(2, 2) - P(1, 2)) ,
+            (u1.x*P1(2, 0) - P1(0, 0)) , (u1.x*P1(2, 1) - P1(0, 1)), (u1.x*P1(2, 2) - P1(0, 2)) ,
+            (u1.y*P1(2, 0) - P1(1, 0)) , (u1.y*P1(2, 1) - P1(1, 1)) , (u1.y*P1(2, 2) - P1(1, 2))  
+            );
+    Mat_<float> B = (Mat_<float>(4, 1) << -(u.x*P(2, 3) - P(0, 3)) ,
+            -(u.y*P(2, 3) - P(1, 3)) ,
+            -(u1.x*P1(2, 3) - P1(0, 3)) ,
+            -(u1.y*P1(2, 3) - P1(1, 3)) 
+            );
+    cv::Mat X;
+    solve(A, B, X, DECOMP_SVD);
+    //solve using opencv done
+    
+    cout << "u" << endl 
+        << u << endl 
+        << "u1" << endl 
+        << u1 << endl 
+        << "P" << endl 
+        << P << endl 
+        << "P1" << endl
+        << P1 << endl 
+        << "A" << endl 
+        << A << endl 
+        << "B" << endl
+        << B << endl 
+        << "X" << endl
+        << X << endl << endl;
+
+    return X;
+}
+
+Mat_<float> IterativeLinearLSTriangulation(Point3f u,    //homogenous image point (u,v,1)
+        Matx34f P,          //camera 1 matrix
+        Point3f u1,         //homogenous image point in 2nd camera
+        Matx34f P1          //camera 2 matrix
+        ) {
+
+    double  EPSILON = 0.000001;
+
+    double wi = 1, wi1 = 1;
+    Mat_<float> X(4, 1);
+
+    for (int i = 0; i < 10; i++) { //Hartley suggests 10 iterations at most
+        Mat_<float> X_ = LinearLSTriangulation(u, P, u1, P1);
+        X(0) = X_(0); X(1) = X_(1); X(2) = X_(2); X(3) = 1.0;
+        //recalculate weights
+        float p2x = Mat_<float>(Mat_<float>(P).row(2)*X)(0);
+        float p2x1 = Mat_<float>(Mat_<float>(P1).row(2)*X)(0);
+
+        //breaking point
+        if (fabsf(wi - p2x) <= EPSILON && fabsf(wi1 - p2x1) <= EPSILON) break;
+
+        wi = p2x;
+        wi1 = p2x1;
+
+        //reweight equations and solve
+        Matx43f A((u.x*P(2, 0) - P(0, 0)) / wi, (u.x*P(2, 1) - P(0, 1)) / wi, (u.x*P(2, 2) - P(0, 2)) / wi,
+                (u.y*P(2, 0) - P(1, 0)) / wi, (u.y*P(2, 1) - P(1, 1)) / wi, (u.y*P(2, 2) - P(1, 2)) / wi,
+                (u1.x*P1(2, 0) - P1(0, 0)) / wi1, (u1.x*P1(2, 1) - P1(0, 1)) / wi1, (u1.x*P1(2, 2) - P1(0, 2)) / wi1,
+                (u1.y*P1(2, 0) - P1(1, 0)) / wi1, (u1.y*P1(2, 1) - P1(1, 1)) / wi1, (u1.y*P1(2, 2) - P1(1, 2)) / wi1
+                );
+        Mat_<float> B = (Mat_<float>(4, 1) << -(u.x*P(2, 3) - P(0, 3)) / wi,
+                -(u.y*P(2, 3) - P(1, 3)) / wi,
+                -(u1.x*P1(2, 3) - P1(0, 3)) / wi1,
+                -(u1.y*P1(2, 3) - P1(1, 3)) / wi1
+                );
+
+        solve(A, B, X_, DECOMP_SVD);
+        X(0) = X_(0); X(1) = X_(1); X(2) = X_(2); X(3) = 1.0;
+    }
+
+    return X;
+}
+
+//Triagulate points
+void TriangulatePoints(const vector<Point2f>& pt_set1,
+                       const vector<Point2f>& pt_set2,
+                       const Matx33f& K,
+                       const Matx34f& P,
+                       const Matx34f& P1,
+                       vector<Point3f>& pointcloud)
+{
+    pointcloud.clear();
+    cout << "Triangulating...";
+    double t = getTickCount();
+    unsigned int pts_size = pt_set1.size();
+    for (unsigned int i=0; i < pts_size; i++){
+        Point2f kp = pt_set1[i];
+        Point3f u(kp.x,kp.y,1.0);
+        Point2f kp1 = pt_set2[i];
+        Point3f u1(kp1.x,kp1.y,1.0);
+        Mat X = IterativeLinearLSTriangulation(u,K*P,u1,K*P1);
+        pointcloud.push_back(Point3f(X.at<float>(0),X.at<float>(1),X.at<float>(2)));
+    }
+    t = ((double)getTickCount() - t)/getTickFrequency();
+    cout << "Done."<< endl;
+}
+
+void triangulate(std::vector<cv::Point2f> &f1_pt, std::vector<cv::Point2f> &f2_pt, std::vector<cv::Mat> &Rs, std::vector<cv::Mat> &ts, std::vector<cv::Point3f> &result){
+    cv::Mat p4s1, p4s2, p4s3, p4s4;
+    std::vector<cv::Point3f> pt_3ds1, pt_3ds2, pt_3ds3, pt_3ds4;
+
+    float fx= 616.662852;
+    float fy= 618.719985;
+    float cx= 323.667190;
+    float cy= 236.399432;
+
+    float cameraArray[9] = 
+    {
+        fx, 0 , cx,
+        0,  fy, cy,
+        0,  0,  1
+    };
+    cv::Mat cameraMatrix(3, 3, CV_32FC1, cameraArray);
+
+    float projArray1[12] = 
+    {
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0
+    }; 
+    cv::Mat projMat1(3, 4, CV_32FC1, projArray1);
+    //projMat1 = cameraMatrix * projMat1;
+    std::cout << projMat1 << std::endl <<std::endl;
+    //cv::Mat cameraMatrixInv = cameraMatrix.inv(DECOMP_SVD);
+
+    cv::Mat projMat2_1(3,4,CV_32FC1), 
+        projMat2_2(3,4,CV_32FC1), 
+        projMat2_3(3,4,CV_32FC1), 
+        projMat2_4(3,4,CV_32FC1);
+
+    std::vector< cv::Mat > projMat2_;
+    projMat2_.push_back(projMat2_1);
+    projMat2_.push_back(projMat2_2);
+    projMat2_.push_back(projMat2_3);
+    projMat2_.push_back(projMat2_4);
+
+    Rs[0].copyTo(projMat2_[0](cv::Rect(0,0,3,3)));
+    ts[0].copyTo(projMat2_[0](cv::Rect(3,0,1,3)));
+    std::cout << projMat2_[0] << std::endl << std::endl;
+    //projMat2_1 = cameraMatrix * projMat2_1;
+    //std::cout << projMat2_1 << std::endl << std::endl;
+
+    Rs[0].copyTo(projMat2_[1](cv::Rect(0,0,3,3)));
+    ts[1].copyTo(projMat2_[1](cv::Rect(3,0,1,3)));
+    std::cout << projMat2_[1] << std::endl << std::endl;
+    //projMat2_2 = cameraMatrix * projMat2_2;
+    //std::cout << projMat2_2 << std::endl << std::endl;
+
+    Rs[1].copyTo(projMat2_[2](cv::Rect(0,0,3,3)));
+    ts[0].copyTo(projMat2_[2](cv::Rect(3,0,1,3)));
+    std::cout << projMat2_[2] << std::endl << std::endl;
+    //projMat2_3 = cameraMatrix * projMat2_3;
+    //std::cout << projMat2_3 << std::endl << std::endl;
+
+    Rs[1].copyTo(projMat2_[3](cv::Rect(0,0,3,3)));
+    ts[1].copyTo(projMat2_[3](cv::Rect(3,0,1,3)));
+    std::cout << projMat2_[3] << std::endl << std::endl;
+    //projMat2_4 = cameraMatrix * projMat2_4;
+    //std::cout << projMat2_4 << std::endl << std::endl;
+
+    //cv::triangulatePoints(projMat1, projMat2_1, f1_pt, f2_pt, p4s1);
+    //cv::triangulatePoints(projMat1, projMat2_2, f1_pt, f2_pt, p4s2);
+    //cv::triangulatePoints(projMat1, projMat2_3, f1_pt, f2_pt, p4s3);
+    //cv::triangulatePoints(projMat1, projMat2_4, f1_pt, f2_pt, p4s4);
+
+    //getPointsFromHomogeneous(p4s1, pt_3ds1);
+    //getPointsFromHomogeneous(p4s2, pt_3ds2);
+    //getPointsFromHomogeneous(p4s3, pt_3ds3);
+    //getPointsFromHomogeneous(p4s4, pt_3ds4);
+    
+    TriangulatePoints(f1_pt, f2_pt, cameraMatrix, projMat1, projMat2_[0], pt_3ds1);
+    TriangulatePoints(f1_pt, f2_pt, cameraMatrix, projMat1, projMat2_[1], pt_3ds2);
+    TriangulatePoints(f1_pt, f2_pt, cameraMatrix, projMat1, projMat2_[2], pt_3ds3);
+    TriangulatePoints(f1_pt, f2_pt, cameraMatrix, projMat1, projMat2_[3], pt_3ds4);
+
+
+    std::vector< std::vector<cv::Point3f> > results;
+    results.push_back(pt_3ds1);
+    results.push_back(pt_3ds2);
+    results.push_back(pt_3ds3);
+    results.push_back(pt_3ds4);
+
+    for(int i = 0; i < 4; i++){
+        if(Point3fRight(results[i], projMat2_[i])){
+            printf("pts_3d %d valid\n", i);
+            result = results[i];
+        }
+        else
+            printf("pts_3d %d invalid\n", i);
+
+        cout << results[i] << endl;
+    }
+
 }
 
 int main(int argc, char** argv )
@@ -231,6 +463,7 @@ int main(int argc, char** argv )
     //draw orb result
     drawMatchResult("orb result", frame1, frame2, out1, out2);
 
+
     //get essentialmat
     cv::Mat essentialmat;
     cv::Mat inliers;
@@ -240,10 +473,8 @@ int main(int argc, char** argv )
     std::vector<cv::Mat> Rs, ts;
     getRt(essentialmat, Rs, ts); 
 
-    //triangulate
-    cv::Mat p4s1, p4s2, p4s3, p4s4;
+
     std::vector<cv::Point2f> f1_pt, f2_pt;
-    std::vector<cv::Point3f> pt_3ds1, pt_3ds2, pt_3ds3, pt_3ds4;
     for(int i= 0 ,_end = inliers.rows; i < _end; i++){
         if(inliers.data[i]){
             f1_pt.push_back(out1[i]->pt);
@@ -251,93 +482,14 @@ int main(int argc, char** argv )
         }
     }
 
-    double fx= 616.662852;
-    double fy= 618.719985;
-    double cx= 323.667190;
-    double cy= 236.399432;
-
-    double cameraArray[9] = 
-    {
-        fx, 0 , cx,
-        0,  fy, cy,
-        0,  0,  1
-    };
-    cv::Mat cameraMatrix(3, 3, CV_32FC1, cameraArray);
-
-    float projArray1[12] = 
-    {
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0
-    }; 
-    cv::Mat projMat1(3, 4, CV_32FC1, projArray1);
-    projMat1 = cameraMatrix * projMat1;
-    std::cout << projMat1 << std::endl <<std::endl;
-
-    cv::Mat projMat2_1(3,4,CV_32FC1), 
-        projMat2_2(3,4,CV_32FC1), 
-        projMat2_3(3,4,CV_32FC1), 
-        projMat2_4(3,4,CV_32FC1);
-
-    Rs[0].copyTo(projMat2_1(cv::Rect(0,0,3,3)));
-    ts[0].copyTo(projMat2_1(cv::Rect(3,0,1,3)));
-    std::cout << projMat2_1 << std::endl << std::endl;
-    projMat2_1 = cameraMatrix * projMat2_1;
-    std::cout << projMat2_1 << std::endl << std::endl;
-
-    Rs[0].copyTo(projMat2_2(cv::Rect(0,0,3,3)));
-    ts[1].copyTo(projMat2_2(cv::Rect(3,0,1,3)));
-    std::cout << projMat2_2 << std::endl << std::endl;
-    projMat2_2 = cameraMatrix * projMat2_2;
-    std::cout << projMat2_2 << std::endl << std::endl;
-
-    Rs[1].copyTo(projMat2_3(cv::Rect(0,0,3,3)));
-    ts[0].copyTo(projMat2_3(cv::Rect(3,0,1,3)));
-    std::cout << projMat2_3 << std::endl << std::endl;
-    projMat2_3 = cameraMatrix * projMat2_3;
-    std::cout << projMat2_3 << std::endl << std::endl;
-
-    Rs[1].copyTo(projMat2_4(cv::Rect(0,0,3,3)));
-    ts[1].copyTo(projMat2_4(cv::Rect(3,0,1,3)));
-    std::cout << projMat2_4 << std::endl << std::endl;
-    projMat2_4 = cameraMatrix * projMat2_4;
-    std::cout << projMat2_4 << std::endl << std::endl;
-
-    //std::cout << f1_ptMat.rows << std::endl;
-    //std::cout << f2_ptMat.rows << std::endl;
-
-    cv::triangulatePoints(projMat1, projMat2_1, f1_pt, f2_pt, p4s1);
-    cv::triangulatePoints(projMat1, projMat2_2, f1_pt, f2_pt, p4s2);
-    cv::triangulatePoints(projMat1, projMat2_3, f1_pt, f2_pt, p4s3);
-    cv::triangulatePoints(projMat1, projMat2_4, f1_pt, f2_pt, p4s4);
-
-    getPointsFromHomogeneous(p4s1, pt_3ds1);
-    getPointsFromHomogeneous(p4s2, pt_3ds2);
-    getPointsFromHomogeneous(p4s3, pt_3ds3);
-    getPointsFromHomogeneous(p4s4, pt_3ds4);
-
-    if(Point3fRight(pt_3ds1))
-        printf("pt_3ds1 valid\n");
-    else
-        printf("pt_3ds1 invalid\n");
-
-    if(Point3fRight(pt_3ds2))
-        printf("pt_3ds2 valid\n");
-    else
-        printf("pt_3ds2 invalid\n");
-
-    if(Point3fRight(pt_3ds3))
-        printf("pt_3ds3 valid\n");
-    else
-        printf("pt_3ds3 invalid\n");
-
-    if(Point3fRight(pt_3ds4))
-        printf("pt_3ds4 valid\n");
-    else
-        printf("pt_3ds4 invalid\n");
-
+    //triangulate
+    std::vector<cv::Point3f> pts_3d;
+    triangulate(f1_pt, f2_pt, Rs, ts, pts_3d);
+    
+    
+    
     PCLView view;
-    view.showRGBPoints(frame1, f1_pt, pt_3ds1);
+    view.showRGBPoints(frame1, f1_pt, pts_3d);
 
     //opencv feature extract
     cv::Ptr<cv::FeatureDetector> _detector;
